acdd03229ed8e607caf0ee4144d53ed15eb5d630
gdcrisanto <gdcrisanto@gmail.com>
2021-04-22 10:29:48 +0800

    copyright inserted

--- src/Home/HomeContact.js
+++ src/Home/HomeContact.js
@@ -2,13 +2,18 @@ import "./Home.scss";
 function HomeContact() {
   return (
     <div className="row home-contact" id="contact">
-      <div className="row contact-title">
-          <h1>Get in touch</h1>
+      <div className="row contact-container">
+        <div className="row contact-title">
+            <h1>Get in touch</h1>
+        </div>
+        <div className="row contact-subtitle">
+          Interested in working together?<br/> Feel free to message me or send an email.
+        </div>
+        <div className="row contact-social">
+        </div>
       </div>
-      <div className="row contact-subtitle">
-        Interested in working together?<br/> Feel free to message me or send an email.
-      </div>
-      <div className="row contact-social">
+      <div className="row contact-copyright">
+        &copy; Gian Diego Crisanto
       </div>
     </div>
   );
